f9422787bfb9006701ab053729843f519674a6f3
root <root@d1stkfactory>
2014-06-27 20:00:40 +0000

    version 1.0 polling with only route 80 (briefcase edition)

--- index.php
+++ index.php
@@ -5,28 +5,72 @@
 <title>MCTS Map</title>
 <style type="text/css">
 	html,body { height: 100%; margin: 0; padding: 0 }
-	#map-canvas { height: 100% }
+	#map-canvas { height: 500px; width: 500px }
 </style>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>    
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/socket.io/0.9.16/socket.io.min.js"></script>
+<script src="//cdn.socket.io/socket.io-1.0.0.js"></script>
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCd4CDT5fgcRK8Ur_VbOpk6RSRrCoZ4kTg"></script>
 <script type="text/javascript">
-var socket = io.connect('wss://corpulenthorse.com:8001');	
+var socket = io.connect('http://corpulenthorse.com:8001');	
+var vehicles = {};
+var map = null;
 $(document).ready(function() {
 	var mapOptions = {
-		center: new google.maps.LatLng(-34.397, 150.644),
-		zoom: 8
+		center: new google.maps.LatLng(43.0500, -87.9500),
+		zoom: radiusToZoom(10)
 	};
-	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+	$("#route").change(function() {
+		socket.emit("getRoute", { rt: $(this).val() });
+	});
+	$("#radius").change(function() {
+		map.setZoom(radiusToZoom($(this).val()));
+	});
 	socket.emit("getRoute", { rt: 80 });
 	socket.on("getRoute", function(data) {
 		$("#json").text(JSON.stringify(data));
+		if ($.isArray(data)) {
+		for(var x=0; x<data.length; x++) {
+			if (vehicles[data[x].vid])
+				vehicles[data[x].vid].marker.setPosition(new google.maps.LatLng(data[x].lat,data[x].lon));
+			else {
+				vehicles[data[x].vid] = data[x];
+				vehicles[data[x].vid].marker = new google.maps.Marker({
+				icon: "css/img/bus.svg",
+				position: new google.maps.LatLng(data[x].lat,data[x].lon),
+				map: map,
+				title: data[x].rt + " - " + data[x].spd + " MPH"
+				});
+			}
+	
+			//vehicles[data[x].vid].marker.getIcon().setAttribute("tranform","rotate(" + data[x].hdg +")");
+		}
+		}
 	});
 });
+function radiusToZoom(radius){
+	radius = (+radius);
+	return Math.round(14-Math.log(radius)/Math.LN2);
+}
 </script>
 </head>
 <body>
-	<div id="map-canvas" />
-	<div id="json"></div>
+	<div>
+		Route <select id="route">
+			<option value="80">80</option>
+			<option value="21">21</option>
+			<option value="15">15</option>
+			<option value="27">27</option>
+		</select> 
+		Radius <select id="radius">
+			<option value="10">10</option>
+			<option value="5">5</option>
+			<option value="20">20</option>
+		</select>
+	</div>
+			
+	
+	<div id="map-canvas"></div>
+	<div id="json">json div</div>
 </body>
 </html>
